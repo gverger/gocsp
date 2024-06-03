9ce0c3c177480221f13c966b2d587cf2b23f9748
Guillaume Verger <verger.guillaume@gmail.com>
2024-06-03 19:30:26 +0000

    sparseset min and max

--- sparseset/sparseset.go
+++ sparseset/sparseset.go
@@ -7,9 +7,9 @@ type Set struct {
 	n      int
 	offset int
 
-	Size int
-	Min  int
-	Max  int
+	Size int // nb of values
+	min  int // min value (before offset)
+	max  int // max value (before offset)
 }
 
 // Create a set of n consecutive values starting at offset
@@ -28,9 +28,23 @@ func NewSet(n int, offset int) *Set {
 		n:      n,
 		offset: offset,
 		Size:   n,
-		Min:    offset,
-		Max:    n + offset - 1,
+		min:    0,
+		max:    n - 1,
 	}
+}
+
+func (s Set) Min() int {
+	if s.IsEmpty() {
+		panic("empty set")
+	}
+	return s.min + s.offset
+}
+
+func (s Set) Max() int {
+	if s.IsEmpty() {
+		panic("empty set")
+	}
+	return s.max + s.offset
 }
 
 // Return all values of the set in order
@@ -45,23 +59,76 @@ func (s Set) AllValues() []int {
 	return values
 }
 
-func (s *Set) Remove(value int) {
+// Remove the value from the set
+func (s *Set) Remove(value int) bool {
 	if !s.Contains(value) {
-		return
+		return false
 	}
 
 	value -= s.offset
 	s.exchangeValues(s.index[value], s.Size-1)
 
 	s.Size--
+	s.updateBoundsValRemoved(value)
+	return true
 }
 
+// Return true if the value is in the set
 func (s *Set) Contains(value int) bool {
 	value -= s.offset
+	return s.contains(value)
+}
+
+func (s *Set) contains(value int) bool {
 	if value < 0 || value >= s.n {
 		return false
 	}
 	return s.index[value] < s.Size
+}
+
+func (s *Set) IsEmpty() bool {
+	return s.Size == 0
+}
+
+// update min and max.
+// Warning val is already shifted
+func (s *Set) updateBoundsValRemoved(val int) {
+	s.updateMaxValRemoved(val)
+	s.updateMinValRemoved(val)
+}
+
+func (s *Set) updateMaxValRemoved(val int) {
+	if s.IsEmpty() || s.max != val {
+		return
+	}
+
+	if s.contains(val) {
+		panic("updateMaxValRemoved")
+	}
+
+	for v := val - 1; v >= s.min; v-- {
+		if s.contains(v) {
+			s.max = v
+			return
+		}
+	}
+}
+
+func (s *Set) updateMinValRemoved(val int) {
+	if s.IsEmpty() || s.min != val {
+		return
+	}
+
+	if s.contains(val) {
+		panic("updateMinValRemoved")
+	}
+
+	for v := val + 1; v <= s.max; v++ {
+		if s.contains(v) {
+			s.min = v
+			return
+		}
+	}
 }
 
 func (s *Set) exchangeValues(a, b int) {
